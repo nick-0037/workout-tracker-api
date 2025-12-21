a40769a21038855a028bf55d4e435ca1036574c3
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-12-21 17:41:01 -0400

    refactor: update OAuth2 token URL

--- api/dependencies/auth.py
+++ api/dependencies/auth.py
@@ -1,10 +1,10 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from api.config import SECRET_KEY, ALGORITHM
 from api.models.user import TokenData
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login/oauth")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
